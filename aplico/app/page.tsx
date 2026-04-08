9f029aebef60b1e70e591ce091522a7e2073cfe0
Hajar <hajar.nasr.dev@gmail.com>
2026-04-08 22:49:28 +0200

    build: install shadcn

--- aplico/app/page.tsx
+++ aplico/app/page.tsx
@@ -1,8 +1,30 @@
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card";
+
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16">
-        <h1>Hello World!</h1>
+        <Card className="w-87.5">
+          <CardHeader>
+            <CardTitle>Workout Plan</CardTitle>
+            <CardDescription>Start your routine today</CardDescription>
+          </CardHeader>
+
+          <CardContent>
+            <p>Track your habits and stay consistent.</p>
+          </CardContent>
+
+          <CardFooter>
+            <button className="text-sm text-blue-500">Get Started</button>
+          </CardFooter>
+        </Card>
       </main>
     </div>
   );
