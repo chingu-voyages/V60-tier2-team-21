cde841dba85f075b5a88221243d6f9fe84d4a9e7
Deniz <zenid@tuta.io>
2026-04-16 16:02:37 +0200

    feat/#35: implement AddApplicationForm with basic validation and state handling

--- components/AddApplicationForm.tsx
+++ components/AddApplicationForm.tsx
@@ -1,16 +1,102 @@
+"use client";
+
+import { useState } from "react";
+
+import { Button } from "./ui/button";
 import { Card, CardContent } from "./ui/card";
 import { Input } from "./ui/input";
 
 export default function AddApplicationForm() {
+  const [applicationFormData, setApplicationFormData] = useState({
+    companyName: "",
+    role: "",
+    date: "",
+    location: "",
+    status: "Applied",
+    notes: "",
+  });
+
+  function handleChange(
+    e: React.ChangeEvent<
+      HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
+    >,
+  ) {
+    setApplicationFormData({
+      ...applicationFormData,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  function handleFormSubmit(e: React.FormEvent) {
+    e.preventDefault();
+
+    console.log("Form Submitted with", applicationFormData);
+  }
+
   return (
-    <Card>
-      <CardContent className="flex flex-col gap-2">
-        <Input placeholder="Company name" />
-        <Input placeholder="Role" />
-        <Input placeholder="Date Applied" />
-        <Input placeholder="Location" />
-        <Input placeholder="Status" />
-        <Input placeholder="Notes" />
+    <Card className="w-full max-w-md">
+      <CardContent>
+        <form className="flex flex-col gap-3" onSubmit={handleFormSubmit}>
+          <label htmlFor="company-name">Company Name</label>
+          <Input
+            name="companyName"
+            id="company-name"
+            type="text"
+            placeholder="e.g ABC Corp."
+            required
+            onChange={handleChange}
+          />
+          <label htmlFor="role">Role</label>
+          <Input
+            name="role"
+            id="role"
+            type="text"
+            placeholder="e.g. Project Manager"
+            required
+            onChange={handleChange}
+          />
+          <label htmlFor="date">Date Applied</label>
+          <Input
+            name="date"
+            id="date"
+            type="date"
+            placeholder="Date Applied"
+            required
+            onChange={handleChange}
+          />
+          <label htmlFor="location">Location</label>
+          <Input
+            name="location"
+            id="location"
+            type="text"
+            placeholder="e.g. Remote (USA)"
+            onChange={handleChange}
+          />
+          <label htmlFor="status">Status</label>
+
+          <select
+            className="block"
+            id="status"
+            name="status"
+            defaultValue={applicationFormData.status}
+            required
+            onChange={handleChange}
+          >
+            <option value="Applied">Applied</option>
+            <option value="Interview">Interview</option>
+            <option value="Offer">Offer</option>
+            <option value="Rejected">Rejected</option>
+          </select>
+          <label htmlFor="notes">Notes</label>
+          <textarea
+            name="notes"
+            id="notes"
+            placeholder="My thoughts..."
+            className="block"
+            onChange={handleChange}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
       </CardContent>
     </Card>
   );
