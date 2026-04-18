af68c98d965d5cc1588c9b9894d23850cba7043d
ZahraSoley <zahra.soleymani.dev@gmail.com>
2026-04-18 22:54:47 +0200

    removed application data table from main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import ApplicationsDataTable from "@/components/ApplicationsDataTable";
 import {
   Card,
   CardContent,
@@ -11,8 +10,8 @@ import {
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center">
-      {/* <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16"> */}
-      {/* <Card className="w-87.5">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16">
+        <Card className="w-87.5">
           <CardHeader>
             <CardTitle>Workout Plan</CardTitle>
             <CardDescription>Start your routine today</CardDescription>
@@ -27,11 +26,8 @@ export default function Home() {
               Get Started
             </button>
           </CardFooter>
-        </Card> */}
-      {/* </main> */}
-      <div>
-        <ApplicationsDataTable />
-      </div>
+        </Card>
+      </main>
     </div>
   );
 }
