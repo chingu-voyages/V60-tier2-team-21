32941bca88c02b958bf04eca46a5a65798cb3388
Hajar <hajar.nasr.dev@gmail.com>
2026-04-19 16:54:59 +0200

    feature: persist applications to localStorage

--- store/applications/useApplicationsStore.tsx
+++ store/applications/useApplicationsStore.tsx
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
 import { INITIAL_APPLICATIONS } from "./data";
 import type { Application, ApplicationsState } from "./types";
@@ -18,16 +19,35 @@ const updateApplication = (
   state.applications[id] = { ...state.applications[id], ...payload };
 };
 
+const getInitialData = () => {
+  if (typeof window === "undefined") return [];
+
+  try {
+    const storeInLocalStorage = JSON.parse(
+      localStorage.getItem("store") || "null",
+    );
+
+    return storeInLocalStorage?.state?.applications || INITIAL_APPLICATIONS;
+  } catch {
+    return INITIAL_APPLICATIONS;
+  }
+};
+
 const useApplicationsStore = create<ApplicationsState>()(
-  immer((set) => ({
-    applications: INITIAL_APPLICATIONS,
-    addApplication: (application: Application) =>
-      set((state) => addApplication(state, application)),
-    updateApplication: (id: string, payload: Record<string, string>) =>
-      set((state) => updateApplication(state, id, payload)),
-    removeApplication: (id: string) =>
-      set((state) => removeApplication(state, id)),
-  })),
+  persist(
+    immer((set) => ({
+      applications: getInitialData(),
+      addApplication: (application: Application) =>
+        set((state) => addApplication(state, application)),
+      updateApplication: (id: string, payload: Record<string, string>) =>
+        set((state) => updateApplication(state, id, payload)),
+      removeApplication: (id: string) =>
+        set((state) => removeApplication(state, id)),
+    })),
+    {
+      name: "store",
+    },
+  ),
 );
 
 export default useApplicationsStore;
