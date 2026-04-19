b17a996668b6700249031e68ff6dbb1047500093
Deniz <zenid@tuta.io>
2026-04-19 10:44:42 +0200

    refactor/#35: use @ alias for imports in ApplicationForm

--- components/ApplicationForm.tsx
+++ components/ApplicationForm.tsx
@@ -2,10 +2,10 @@
 
 import { useState } from "react";
 
-import { Button } from "./ui/button";
-import { Card, CardContent } from "./ui/card";
-import { Input } from "./ui/input";
-import { Label } from "./ui/label";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import {
   Select,
   SelectContent,
