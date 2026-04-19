f3646828b1a05b4ba59977e1f1bd49a595b7c0d6
fabirez <skyc4ff3ine@virgilio.it>
2026-04-19 17:34:56 +0200

    feat: Added application details with minimum design

--- app/application/[id]/page.tsx
+++ app/application/[id]/page.tsx
@@ -1,15 +1,21 @@
+import { ChevronLeft } from "lucide-react";
 import Link from "next/link";
 import Application from "@/components/applications/Application";
+import { Button } from "@/components/ui/button";
 
 const ApplicationPage = async ({ params }: { params: { id: string } }) => {
   const { id } = await params;
 
   return (
     <>
-      <Application id={id} />
-      <Link href="/applications" className="underline">
-        All Applications
+      <Link
+        href="/applications"
+        className="mb-8 text-muted-foreground flex items-center gap-2 text-sm"
+      >
+        <ChevronLeft className="size-4" />
+        Applications
       </Link>
+      <Application id={id} />
     </>
   );
 };
--- components/applications/Application.tsx
+++ components/applications/Application.tsx
@@ -1,18 +1,80 @@
 "use client";
 
+import { ArrowLeft, Hash, Map as MapIcon, Pencil, Trash2 } from "lucide-react";
+import Link from "next/link";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import useApplicationsStore from "@/store/applications/useApplicationsStore";
 
 const Application = ({ id }: { id: string }) => {
   const application = useApplicationsStore((state) => state.applications[id]);
 
-  if (!application) return <p>Application Not Found</p>;
+  if (!application) {
+    // TODO: improve this
+    return (
+      <div className="flex flex-col items-center pt-40">
+        <p className="text-foreground pb-2"> Not found</p>
+        <Link href="/applications">
+          <Button variant="outline" className="text-muted-foreground">
+            <ArrowLeft className="size-4" />
+            Applications
+          </Button>
+        </Link>
+      </div>
+    );
+  }
 
   return (
     <div>
-      <p>{application.id}</p>
-      <p>{application.role}</p>
-      <p>{application.companyName}</p>
-      <p>{application.status}</p>
+      <header className="flex justify-between items-center">
+        <div>
+          <div className="flex gap-4 items-center">
+            <h1 className="text-2xl font-semibold">
+              {application.companyName}
+            </h1>
+            <Badge variant={application.status}>
+              <p>{application.status}</p>
+            </Badge>
+          </div>
+
+          <p className="text-lg text-muted-foreground font-medium pb-3">
+            {application.role}
+          </p>
+        </div>
+
+        <div className="flex gap-4">
+          <Button variant="outline">
+            <Pencil className="size-4" />
+            Edit
+          </Button>
+          <Button variant="destructive">
+            <Trash2 className="size-4" />
+            Delete
+          </Button>
+        </div>
+      </header>
+
+      <div className="flex gap-6 items-center pb-12 text-base">
+        <div className="flex gap-3 items-center">
+          <MapIcon className="size-4 text-muted-foreground" />
+          <span>{application.location}</span>
+        </div>
+
+        <div className="flex gap-3 items-center">
+          <Hash className="size-4 text-muted-foreground" />
+          <span>{application.id}</span>
+        </div>
+      </div>
+
+      <Card className="max-w-(--breakpoint-xl)">
+        <CardHeader>
+          <CardTitle className="text-lg">Notes</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div className="whitespace-pre-wrap text-sm">{application.notes}</div>
+        </CardContent>
+      </Card>
     </div>
   );
 };
