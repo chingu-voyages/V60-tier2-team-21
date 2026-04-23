f726ced9ca394c53ec30db1bd3c0b15e590bc759
Deniz <zenid@tuta.io>
2026-04-23 16:49:55 +0200

    feat/#39: add nav links with icons to app layout.

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,3 +1,9 @@
+import {
+  ChartColumnBig,
+  CirclePlus,
+  Folder,
+  LayoutDashboard,
+} from "lucide-react";
 import Link from "next/link";
 
 export default function AppLayout({
@@ -5,22 +11,27 @@ export default function AppLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const navLinks = [
+    { href: "/dashboard", name: "Dashboard", icon: LayoutDashboard },
+    { href: "/applications", name: "Applications", icon: Folder },
+    { href: "/new-entry", name: "New Entry", icon: CirclePlus },
+    { href: "/analytics", name: "Analytics", icon: ChartColumnBig },
+  ];
+
+  const sideBarElements = navLinks.map((navLink) => {
+    return (
+      <li key={navLink.href}>
+        <Link className="flex gap-1" href={navLink.href}>
+          <navLink.icon />
+          {navLink.name}
+        </Link>
+      </li>
+    );
+  });
+
   return (
     <div>
-      <ul>
-        <li>
-          <Link href="/dashboard">Dashboard</Link>
-        </li>
-        <li>
-          <Link href="/applications">Applications</Link>
-        </li>
-        <li>
-          <Link href="/new-entry">New Entry</Link>
-        </li>
-        <li>
-          <Link href="/analytics">Analytics</Link>
-        </li>
-      </ul>
+      <ul>{sideBarElements}</ul>
       {children}
     </div>
   );
