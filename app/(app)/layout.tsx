be0250ff998b7e210d63ef107ca9388e245b1946
Deniz <zenid@tuta.io>
2026-04-23 17:14:31 +0200

    refactor/#39: replace fixed sidebar with CSS grid layout.

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -31,12 +31,12 @@ export default function AppLayout({
   });
 
   return (
-    <div className="flex min-h-screen">
-      <div className="fixed left-0 top-0 h-screen border-amber-50 py-4 px-6">
+    <div className="grid grid-cols-[256px_1fr] min-h-screen">
+      <div className="h-screen border-r px-4 py-6">
         <Placeholderlogo className="mb-6" />
         <ul className="flex flex-col gap-4">{sideBarElements}</ul>
       </div>
-      <main className="ml-64 flex-1 py-4">{children}</main>
+      <main className="mx-auto w-full max-w-2xl py-4">{children}</main>
     </div>
   );
 }
