6e49470eada49adbd10bb38e02f2756c595a8a89
Deniz <zenid@tuta.io>
2026-04-24 20:43:55 +0200

    chore/#39: extract sidebar to its own component.

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,40 +1,16 @@
-import {
-  ChartColumnBig,
-  CirclePlus,
-  Folder,
-  LayoutDashboard,
-} from "lucide-react";
-import Link from "next/link";
 import Placeholderlogo from "@/components/PlaceholderLogo";
+import Sidebar from "@/components/Sidebar";
 
 export default function AppLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const navLinks = [
-    { href: "/dashboard", name: "Dashboard", icon: LayoutDashboard },
-    { href: "/applications", name: "Applications", icon: Folder },
-    { href: "/new-entry", name: "New Entry", icon: CirclePlus },
-    { href: "/analytics", name: "Analytics", icon: ChartColumnBig },
-  ];
-
-  const sideBarElements = navLinks.map((navLink) => {
-    return (
-      <li key={navLink.href}>
-        <Link className="flex gap-3" href={navLink.href}>
-          <navLink.icon />
-          {navLink.name}
-        </Link>
-      </li>
-    );
-  });
-
   return (
     <div className="grid grid-cols-[256px_1fr] min-h-screen">
       <div className="h-screen border-r px-4 py-6">
         <Placeholderlogo className="mb-6" />
-        <ul className="flex flex-col gap-4">{sideBarElements}</ul>
+        <Sidebar />
       </div>
       <main className="mx-auto w-full max-w-2xl py-4">{children}</main>
     </div>
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,3 +1,30 @@
+import {
+  ChartColumnBig,
+  CirclePlus,
+  Folder,
+  LayoutDashboard,
+} from "lucide-react";
+
+import Link from "next/link";
+
 export default function Sidebar() {
-  return <p>I am Sidebar</p>;
+  const navLinks = [
+    { href: "/dashboard", name: "Dashboard", icon: LayoutDashboard },
+    { href: "/applications", name: "Applications", icon: Folder },
+    { href: "/new-entry", name: "New Entry", icon: CirclePlus },
+    { href: "/analytics", name: "Analytics", icon: ChartColumnBig },
+  ];
+
+  const sideBarElements = navLinks.map((navLink) => {
+    return (
+      <li key={navLink.href}>
+        <Link className="flex gap-3" href={navLink.href}>
+          <navLink.icon />
+          {navLink.name}
+        </Link>
+      </li>
+    );
+  });
+
+  return <ul className="flex flex-col gap-4">{sideBarElements}</ul>;
 }
